2a57cf2fe408b6bd84b3aedc2a3300d2d545f671
rony <ronypangaribuan2505@gmail.com>
2025-02-27 14:29:32 +0700

    PAIR 23

--- t04_02.c
+++ t04_02.c
@@ -1,3 +1,6 @@
+// 12S24005 - Mia Nathania Sibuea
+// 12S24040 - Rony Reynaldy Pangaribuan
+
 #include <stdio.h>
 #include <string.h>
 
@@ -7,23 +10,23 @@
 int main() {
     char input[MAX_LENGTH + 1]; 
 
-        if (fgets(input, sizeof(input), stdin) != NULL) {
-       
-        input[strcspn(input, "\n")] = 0; 
-    }
+  
+    fgets(input, sizeof(input), stdin);
 
+    input[strcspn(input, "\n")] = 0;
+    
     int length = strlen(input);
     if (length < MIN_LENGTH || length > MAX_LENGTH) {
         printf("Input tidak valid. Panjang harus antara %d dan %d.\n", MIN_LENGTH, MAX_LENGTH);
         return 1;
     }
 
-   
     for (int i = 0; i < length; i++) {
+        
         printf("%03d", (int)input[i]);
     }
 
-    printf("\n"); 
+    printf("013"); 
 
     return 0;
 }
