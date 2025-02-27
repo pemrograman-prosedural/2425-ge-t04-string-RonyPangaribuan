43224b300fed54698e42d970b419e3885de6fbb8
rony <ronypangaribuan2505@gmail.com>
2025-02-27 13:42:34 +0700

    PAIR 23

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,49 @@
-// NIM - Name
-// NIM - Name
+// 12S24005 - Mia Nathania Sibuea
+// 12S24040 - Rony Reynaldy Pangaribuan
 
-#include <stdio.h>
-
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
-}
+  #include <stdio.h>
+  #include <string.h>
+  
+  #define MIN_LENGTH 3
+  #define MAX_LENGTH 60
+  
+  // Fungsi untuk mengonversi string ke integer
+  int string_to_int(const char *str) {
+      int result = 0;
+      for (int i = 0; i < 3; i++) {
+          result = result * 10 + (str[i] - '0'); 
+      }
+      return result;
+  }
+  
+  int main() {
+  char input[MAX_LENGTH + 1]; 
+  
+     
+  printf("Masukkan Icarus Message (3-60 karakter): ");
+  fgets(input, sizeof(input), stdin);
+  
+    
+  input[strcspn(input, "\n")] = 0;
+  
+  int length = strlen(input);
+    if (length < MIN_LENGTH || length > MAX_LENGTH || length % 3 != 0) {
+      printf("Input tidak valid. Panjang harus antara %d dan %d dan harus kelipatan 3.\n", MIN_LENGTH, MAX_LENGTH);
+      return 1;
+      }
+  
+    for (int i = 0; i < length; i += 3) {
+          
+    char ascii_str[4]; 
+    strncpy(ascii_str, input + i, 3);
+    ascii_str[3] = '\0'; 
+  
+          
+    int ascii_value = string_to_int(ascii_str);
+    printf("%c", (char)ascii_value);
+      }
+  
+    printf("\n"); 
+  
+return 0;
+  }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,34 @@
-// NIM - Name
-// NIM - Name
+// 12S24005 - Mia Nathania Sibuea
+// 12S24040 - Rony Reynaldy Pangaribuan
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+#define MIN_LENGTH 1
+#define MAX_LENGTH 20
 
-  return 0;
+int main() {
+    char input[MAX_LENGTH + 1]; 
+
+  
+    printf("Masukkan teks (1-20 karakter): ");
+    fgets(input, sizeof(input), stdin);
+
+    input[strcspn(input, "\n")] = 0;
+
+    
+    int length = strlen(input);
+    if (length < MIN_LENGTH || length > MAX_LENGTH) {
+        printf("Input tidak valid. Panjang harus antara %d dan %d.\n", MIN_LENGTH, MAX_LENGTH);
+        return 1;
+    }
+
+    for (int i = 0; i < length; i++) {
+        
+        printf("%03d", (int)input[i]);
+    }
+
+    printf("\n"); 
+
+    return 0;
 }
